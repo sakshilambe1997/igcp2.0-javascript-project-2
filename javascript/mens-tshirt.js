267dffd8949155558fc350ddb895de594b5b7d3b
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-04-14 20:50:08 +0530

    js code added and card added

--- javascript/mens-tshirt.js
+++ javascript/mens-tshirt.js
@@ -102,6 +102,59 @@ function changeImggreen2(){
     const imageElement =document.getElementById("card-img2")
     imageElement.src= "./../img/mens-img/tshirt-2g.jpg"
 }
+//card 3
+
+const quantityPriceElement3 = document.getElementById("quantity-price3")
+const finalPriceElement3 = document.getElementById("final-price3")
+
+function increaseQuantity3() {
+  const quantityElement = document.getElementById('quantity3');
+
+  let quantityValue = parseInt(quantityElement.innerText);
+  // let priceQuantityValue = parseInt(quantityPriceElement.innerText);
+  if (quantityValue < 5) {
+      quantityElement.innerText = ++quantityValue;
+
+  }
+  else {
+      alert('You can not add more than 5 items');
+
+  }
+  quantityPriceElement3.innerText = `${quantityValue} X 650`;
+  finalPriceElement3.innerText = `Total : ${quantityValue * 650}`;
+
+
+}
+
+function decreaseQuantity3() {
+  const quantityElement = document.getElementById('quantity3');
+  let quantityValue = parseInt(quantityElement.innerText);
+
+  if (quantityValue > 1) {
+      quantityElement.innerText = --quantityValue;
+  }
+  else {
+      alert('You can not add less than 1 items');
+  }
+  quantityPriceElement3.innerText = `${quantityValue} X 650`;
+  finalPriceElement3.innerText = `Total : ${quantityValue * 650}`;
+
+}
+
+function changeImgblue3(){
+  const imageElement =document.getElementById("card-img3")
+  imageElement.src= "./../img/mens-img/tshirt-3b.jpg"
+}
+
+function changeImgred3(){
+  const imageElement =document.getElementById("card-img3")
+  imageElement.src= "./../img/mens-img/tshirt-3r.jpg"
+}
+
+function changeImggray3(){
+  const imageElement =document.getElementById("card-img3")
+  imageElement.src= "./../img/mens-img/tshirt-3g.jpg"
+}
 
 
 
